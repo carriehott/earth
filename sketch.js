f0938b1c785abafc22d197d858202530f1f7d7dd
carriehott <carriehott@gmail.com>
2026-03-25 11:57:24 -0400

    first sketch

--- sketch.js
+++ sketch.js
@@ -1,9 +1,28 @@
+var poetry = ["yogurt", "coffee", "chia seeds", "strawberries", "blueberries", "raspberries", "half & half"]; 
+
+var index = 5; 
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background('red');
-  fill('blue');
-  ellipse(200, 200, 100, 100);
+  background(220);
+  
+  textSize(24); 
+  fill('green'); 
+  text(poetry[index], width/3, height/2); 
+  
 }
+
+function keyPressed() {
+   index = index +1; 
+  
+  if(index == poetry.length) {
+    index = 0; 
+  }
+}
+
+
+
+
